1a7c805695417d5abec388c386d55e5afc6f4946
Mastercelta <juancamilo@bees.com.co>
2025-04-06 18:32:12 -0500

    Configuracion inicial de la api

--- api/app/main.py
+++ api/app/main.py
@@ -1,1 +1,13 @@
 # api/app/main.py
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return jsonify({"message": "API Flask is running!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
