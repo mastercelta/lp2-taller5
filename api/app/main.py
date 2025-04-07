f78eda6f732311f25adfa3ac072e191ffd64288e
Mastercelta <juancamilo@bees.com.co>
2025-04-06 23:48:44 -0500

    Add cors

--- api/app/main.py
+++ api/app/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 import uvicorn
 from app.routers import users
 from app.routers import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(auth.router)
