985cf085680e3956fb2eb1b3afd104186193dcf1
Mastercelta <juancamilo@bees.com.co>
2025-04-08 19:42:18 -0500

    Add posts

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
-from app.routers import users
-from app.routers import auth
+from app.routers import users, comments, auth, posts
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
@@ -20,6 +19,8 @@ app.add_middleware(
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(comments.router)
+app.include_router(posts.router)
 
 
 @app.get("/")
